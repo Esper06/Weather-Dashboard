b18a828c6a217022e736f123129cd219510ee59a
Esper06 <matt.walford98@gmail.com>
2021-07-10 11:18:25 +0100

    added api code to js, also added new file for that api code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -6,3 +6,28 @@
 
 //you'll need two api's   1: openWeatherMap API    2:  one call weather
 
+
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={aa891061c4eb5441b7aab33d1b619398}
+
+function generateURL(city) {
+
+    var NUMBER_OF_DAYS_TO_REQUEST = 5;
+
+    return `api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS_TO_REQUEST}&appid={aa891061c4eb5441b7aab33d1b619398}`
+
+}
+
+function makeRequest(endpoint) {
+    return fetch(endpoint).then(function (res) {
+        return res.json();
+    });
+
+}
+
+var city = "Birmingham"
+
+var endpoint = generateEndpoint(city)
+
+makeRequest(endpoint).then(function(weatherInfo){
+    
+})
