162861c3969c8623a9534f3274b6f9ef7b5a3655
Esper06 <matt.walford98@gmail.com>
2021-07-11 17:48:50 +0100

    pushed changes for assistance

--- Assets/JS/api.js
+++ Assets/JS/api.js
@@ -1,28 +1,38 @@
+
+var endpoint
+
 function generateEndpoint(city) { //this function makes a url from the api to be used. The city and number of days bits are variables
 
     var NUMBER_OF_DAYS_TO_REQUEST = 5; //we hard code the number of days as we only want 5
+    var city = "Birmingham" //here as a test remember to remove
 
-    return `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS_TO_REQUEST}&appid={aa891061c4eb5441b7aab33d1b619398}`
-
+    endpoint = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS_TO_REQUEST}&appid=aa891061c4eb5441b7aab33d1b619398&units=metric`
 }
 
-var endpoint = generateEndpoint(city) //makes it so that endpoint is the url result of generateEndpoint
+//var endpoint = generateEndpoint(city) //makes it so that endpoint is the url result of generateEndpoint
 
 
 //endpoint is the url of the above function, we run a fetch command on it and then return the response as an object
 function makeRequest(endpoint) {
     return fetch(endpoint).then(function (res) { //fetch url result
         return res.json(); //turn result in object
-    });
+    })
+    .then(function(data){
+        console.log(data)
+    })
 
 }
 
 
-var city = "Birmingham" //only here as a test, remember to remove
+generateEndpoint();
+makeRequest();
 
 
+/*
 makeRequest(endpoint).then(function(weatherInfo){ 
     
 });
 
 makeRequest();
+
+*/
--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,10 +1,26 @@
 
 
-//fetch for city
-
-//within that fetch use the co-ordinats to get the future data
-
 //you'll need two api's   1: openWeatherMap API    2:  one call weather
 
+//Grabbing some elements from our html
 
-//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={aa891061c4eb5441b7aab33d1b619398}
+/*
+
+var citySelectionForm = $("#citySelectionForm"); //this selects the entire search bar and button area
+
+var cityTextBox = $("#cityTextBox"); //this specifically targets the text box
+
+
+
+function createWeatherCare(dayWeatherInfo) {
+
+    var temp = dayWeatherInfo.temp;
+    var windSpeed = dayWeatherInfo.windSpeed;
+    var humidity = dayWeatherInfo.humidity;
+
+
+    
+}
+
+*/
+
