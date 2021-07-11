889e4d3c638e874fe9151b6d65afcf1530ff307e
Esper06 <matt.walford98@gmail.com>
2021-07-11 23:37:34 +0100

    deleted api.js and merged the code with script.js, added main card div in html and styled with css

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,26 +1,41 @@
+//Elements grabbed from html
 
+var cityTextBox = document.getElementById("cityTextBox") //selects the search box from html 
+var searchButton = document.getElementById("searchButton") //selects the button from html
+var selectedCity = document.getElementById("mainCardCity") //selects the large text in the main forecast section
+var mainTemp = document.getElementById("mainTemp") //selects the span element inside the list item, inside the main card div 
 
-//you'll need two api's   1: openWeatherMap API    2:  one call weather
+//Some variables we make to be used later on
+var endpoint
+mainTemp = ""
+//this function makes a url from the api to be used. The city is a variable that depends on the user
+function generateEndpoint(city) { 
+    event.preventDefault(); //stops the browser from refreshing the page
 
-//Grabbing some elements from our html
+    var city = cityTextBox.value //sets it so that whatever the client types in is the city that is searched
 
-/*
+                //assigns the url for the api to the variable endpoint
+    endpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=aa891061c4eb5441b7aab33d1b619398&units=metric`
 
-var citySelectionForm = $("#citySelectionForm"); //this selects the entire search bar and button area
-
-var cityTextBox = $("#cityTextBox"); //this specifically targets the text box
-
-
-
-function createWeatherCare(dayWeatherInfo) {
-
-    var temp = dayWeatherInfo.temp;
-    var windSpeed = dayWeatherInfo.windSpeed;
-    var humidity = dayWeatherInfo.humidity;
-
-
-    
+    selectedCity.innerHTML = city
 }
 
-*/
+
+//endpoint is the url of the above function, we run a fetch command on it and then return the response as an object
+function makeRequest() {
+    event.preventDefault();
+
+    
+    return fetch(endpoint).then(function (res) { //fetch url result
+        return res.json(); //turn result in object
+    })
+    .then(function(data){
+        console.log(data)
+        mainTemp.innerText = data.list[0].main.temp //sets the inner html of the main temperature span to be whatever the response of the api is
+    })
+
+}
+
+searchButton.addEventListener('click', generateEndpoint) //creates the url when the button is clicked
+searchButton.addEventListener('click', makeRequest) //makes a request to the url gotten from generateEndpoint once the button is clicked
 
