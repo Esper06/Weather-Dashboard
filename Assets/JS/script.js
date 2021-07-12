9b53e4c12a3ccf9381a52a72ac87b140b856f672
Esper06 <matt.walford98@gmail.com>
2021-07-12 18:47:43 +0100

    Updated JS to now show UV index and colour it in css depending on the severity

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,7 +7,8 @@ var selectedCity = document.getElementById("mainCardCity") //selects the large t
 var mainTemp = document.getElementById("mainTemp") //selects the span element inside the list item, inside the main card div 
 var mainWind = document.getElementById("mainWind") // selects the span element inside the list item, inside the main card div
 var mainHumid = document.getElementById("mainHumid") // same as above 
-var mainDate = document.getElementById("mainCardDate") //selects the span inside the header for the main card
+var mainUV = document.getElementById("mainUV")
+var mainIcon = document.getElementById("mainIcon")
 
 var futTempOne = document.getElementById("futTempOne")
 var futTempTwo = document.getElementById("futTempTwo")
@@ -27,11 +28,18 @@ var futHumidThree = document.getElementById("futHumidThree") //selects the eleme
 var futHumidFour = document.getElementById("futHumidFour")
 var futHumidFive = document.getElementById("futHumidFive")
 
+var iconOne = document.getElementById("iconOne")
+var iconTwo = document.getElementById("iconTwo")
+var iconThree = document.getElementById("iconThree") //selects the img element inside the future forecasts
+var iconFour = document.getElementById("iconFour")
+var iconFive = document.getElementById("iconFive")
+
 //Some variables we make to be used later on
 
 var endpoint
 var date = new Date //grabs todays date
-
+var lat  //sets a new variable for the latitude
+var lon  //sets a new variable for the longitude
 
 //this function makes a url from the api to be used. The city is a variable that depends on the user
 function generateEndpoint(city) { 
@@ -51,11 +59,13 @@ function makeRequest() {
     event.preventDefault();
 
     
-    return fetch(endpoint).then(function (res) { //fetch url result
+    return fetch(endpoint)
+    .then(function (res) { //fetch url result
+
         return res.json(); //turn result in object
     })
     .then(function(data){ //this functions sets the inner html of the elements as the results from the api
-        //console.log(data)
+        console.log(data)
         mainTemp.innerText = data.list[0].main.temp //sets the inner html of the main temperature span to be whatever the response of the api is
         mainWind.innerText = data.list[0].wind.speed // sets the inner html of the main wind span to be whatever the response of the api is
         mainHumid.innerText = data.list[0].main.humidity //same as above but for humidity
@@ -79,9 +89,55 @@ function makeRequest() {
         futTempFive.innerText = data.list[39].main.temp
         futWindFive.innerText = data.list[39].wind.speed   //sets inner html for the fifth card
         futHumidFive.innerText = data.list[39].main.humidity
+
+
+      //here we also set the lon and lat values to be the latitude and longitude of the chosen city
+        lon = data.city.coord.lon
+                                    //This is so I can use the data from this function in another function later
+        lat = data.city.coord.lat
+
+        //Next we use the data to get the icons for the weather so we can display them
+
+        
+        
+    })
+    .then(function getUV() { //this long string of .then statements is so that after we get the lat and lon of the city we can then put it into another api
+        //we set endpointUV to have the value of the new api url, and have the lat and lon variables equal whatever the city lat lon was
+        endpointUV = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=aa891061c4eb5441b7aab33d1b619398`
+        
+        return fetch(endpointUV) //we fetch the data of the new api
+    })
+    .then(function (res) {
+        return res.json(); //turn it into an object
+    })
+    .then(function (data) {
+        //console.log(data)
+
+       
+        mainUV.innerText = data.current.uvi //finally we set the text of mainUV to be the value of uvi in that object
+        
+        if (mainUV.innerText < 6) {
+            
+            mainUV.classList.remove('highUV')
+            mainUV.classList.remove('mediumUV')
+            mainUV.classList.add('lowUV');
+        } 
+        else if (mainUV.innerText < 8) {
+
+            mainUV.classList.remove('lowUV')
+            mainUV.classList.remove('highUV')
+            mainUV.classList.add('mediumUV')
+        }
+         else {
+
+            mainUV.classList.remove('mediumUV')
+            mainUV.classList.remove('lowUV')
+            mainUV.classList.add('highUV')
+        }
     })
 
 }
+
 
 //adds searched citys as a list item to an unordered list under the search bar
 function addToList (city) {
@@ -100,18 +156,63 @@ function addToList (city) {
 
 }
 
+//This functions creates and assigns dates to each of the cards
 function setDates () {
-    var currentDate = new Date;
-    
+    //First we create the date and get it's individual parts
+    var currentDate = new Date(),
     day = currentDate.getDate(),
-    month = currentDate.getMonth() + 1,
+    month = currentDate.getMonth() + 1, //we add 1 to the month as it always seems to be a month behind
     year = currentDate.getFullYear(),
     
-    text = (day + "/" + month + "/" + year);
+    text = (day + "/" + month + "/" + year);  //we put these parts together in an order we choose and assign it to 'text'
     
-    mainDate.innerText = text;
-}
+    document.getElementById("mainCardDate").innerHTML = text; //then we set the variable 'text' to be the innerHTML of the date container
 
+    //Same as above however we're added +1 to the date to show that it's tomorrow
+    day = currentDate.getDate() + 1,
+    month = currentDate.getMonth() + 1,
+    year = currentDate.getFullYear(),
+
+    futTextOne = (day + "/" + month + "/" + year);
+
+    document.getElementById("futDateOne").innerHTML = futTextOne;
+
+    //future card 2
+    day = currentDate.getDate() +2,
+    month = currentDate.getMonth() +1,
+    year = currentDate.getFullYear(),
+
+    futTextTwo = (day + "/" + month + "/" + year);
+
+    document.getElementById("futDateTwo").innerHTML = futTextTwo;
+
+    //future card 3
+    day = currentDate.getDate() +3,
+    month = currentDate.getMonth() +1,
+    year = currentDate.getFullYear(),
+
+    futTextThree = (day + "/" + month + "/" + year);
+
+    document.getElementById("futDateThree").innerHTML = futTextThree;
+
+    //future card 4
+    day = currentDate.getDate() +4,
+    month = currentDate.getMonth() +1,
+    year = currentDate.getFullYear(),
+
+    futTextFour = (day + "/" + month + "/" + year);
+
+    document.getElementById("futDateFour").innerHTML = futTextFour;
+
+    //future card 5
+    day = currentDate.getDate() +5,
+    month = currentDate.getMonth() +1,
+    year = currentDate.getFullYear(),
+
+    futTextFive = (day + "/" + month + "/" + year);
+
+    document.getElementById("futDateFive").innerHTML = futTextFive;
+}
 
 
 searchButton.addEventListener('click', generateEndpoint) //creates the url when the button is clicked
